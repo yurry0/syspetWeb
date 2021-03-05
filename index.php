8459502b1ffbd2c5be80ccf12a4902b8eb5430b7
Yuri Martins <yurimartinsr@gmail.com>
2021-03-05 09:31:43 -0300

    Iniciando Ambiente no GITHUB

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="dist/css/bootstrap.css">
+
+  <title>Hello, world!</title>
+
+</head>
+
+
+<body>
+
+
+
+<h1>Hello, world!</h1>
+<div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+
+
+
+  <!-- jQuery -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="dist/js/demo.js"></script>
+
+
+</body>
+
+</html>
